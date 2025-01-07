c88ab8dcd3f043f70f417aaf4ade9f117e914be4
Vanessa <vanessaquinoo@hotmail.com>
2025-01-07 15:32:22 -0300

    feat: terminado a funcionalidade de todos os metodos de pagamento

--- src/app/boleto/boleto.component.ts
+++ src/app/boleto/boleto.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-boleto',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './boleto.component.html',
   styleUrl: './boleto.component.css'
 })
 export class BoletoComponent {
+
+  @ViewChild('textToCopy') textToCopy!: ElementRef;
+
+  showMessage: boolean = false;
+
+  copyText(event: Event) {
+    event.preventDefault();
+
+    const textElement = this.textToCopy.nativeElement;
+    const textToCopy = textElement.innerText || textElement.textContent;
+
+    if(textToCopy) {
+      navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        this.showFeedbackMessage();
+      })
+      .catch(err => {
+        console.error('Falha ao copiar o código', err);
+      });
+    }
+  }
+  showFeedbackMessage(): void {
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 3000);
+  }
+
+  openPrinter(event: Event): void {
+    event.preventDefault();
+    window.print();
+  }
 
 }
--- src/app/pix/pix.component.ts
+++ src/app/pix/pix.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pix',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pix.component.html',
   styleUrl: './pix.component.css'
 })
 export class PixComponent {
+  @ViewChild('textToCopy') textToCopy!: ElementRef<HTMLInputElement>;
 
+  showMessage: boolean = false;
+
+  copyText(event: Event): void {
+    event.preventDefault();
+
+    const textElement = this.textToCopy.nativeElement;
+    const textToCopy = textElement.value;
+
+    if(textToCopy) {
+      navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        this.showFeedbackMessage();
+      })
+      .catch(err => {
+        console.error('Falha ao copiar o código', err);
+      });
+    }
+}
+  showFeedbackMessage(): void {
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 3000);
+
+    }
 }
